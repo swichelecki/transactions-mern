b05fd7c488ba4b057c3cb9d46a326773745c9a8c
Steve Wichelecki <wichelecki_steve@shure.com>
2020-03-01 13:57:57 -0600

    changed build folder

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use('/api/v1/transactions', transactions);
 app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'));
+  app.use(express.static('build'));
   
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
 
 }
 
